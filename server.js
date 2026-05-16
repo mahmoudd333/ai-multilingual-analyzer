c9ff931f768eac8ebc76bd14b29c2fc220b59fc9
mahmouddughman11-star <mahmouddughman11@gmail.com>
2026-05-16 22:15:18 +0300

    fixing port issue

--- server.js
+++ server.js
@@ -122,7 +122,8 @@ app.post("/analyze", async (req, res) => {
 });
 
 
-// Start Server
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
